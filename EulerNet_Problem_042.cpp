6d3fa03390a448ce15fe6a72bf158a9b03548a20
Holytu <nthuxy12345@gmail.com>
2019-07-29 16:51:44 +0800

    Add files via upload

---
+++ EulerNet_Problem_042.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <string>
+#include <math.h>
+#include <cmath>
+using namespace std;
+int main()
+{
+	fstream file;
+	char word;
+	int count = 0;
+	int TriangleNum = 0;
+	int flag = 0;
+	int test = 0;
+	string inputStr;
+
+	file.open("p042_words.txt", ios::in);
+
+	if(!file)
+	{
+		cout <<"Error\n";
+	}
+	else
+	{
+		while( file.get(word) )
+		{
+			if(word == '"' && flag == 0)
+			{
+				count = 0 ;
+				flag++;
+			}
+			else if(word == '"' && flag == 1)
+			{
+				count = count * 2;
+				int x = sqrt(count);
+				int y = x + 1;
+				if(x * y == count)
+				{
+					TriangleNum++;
+				}
+				
+				flag = 0;
+			}
+			else
+			{
+				count = count + ( word - 'A' ) + 1 ;
+			}
+		}
+		cout << "TriangleNum = " << TriangleNum << endl;
+		
+		file.close();
+	}
+	return 0;
+}
